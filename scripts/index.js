84ecb683a253cd57167b644fbf29abb1bb31cda4
ryanzola <ryanzola@me.com>
2018-02-21 17:56:33 -0500

    cool animation

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,19 @@
 'use strict';
 
 (function() {
-  document.getElementById('message').innerHTML = 'This message and color was set by minified JS'
+
+  var i = 0;
+  var txt = 'This message and color was brought to you by minified JS'; /* The text */
+  var speed = 50; /* The speed/duration of the effect in milliseconds */
+
   document.getElementById('message').style.color = '#FF0000';
+  function typeWriter() {
+    if (i < txt.length) {
+      document.getElementById("message").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  typeWriter();
+  
 })();
